c2e6be078b59b4ee68e518584f7c328f6c169f60
ReltivlyObjectv <me@relativelyobjective.tech>
2017-07-12 14:08:12 -0700

    Initial Commit

---
+++ tech/relativelyobjective/monsterbrewery/MonsterBrewery.java
@@ -0,0 +1,13 @@
+package tech.relativelyobjective.monsterbrewery;
+
+/**
+ *
+ * @author Pengu
+ * Contact: me@relativelyobjective.tech
+ * 
+ */
+public class MonsterBrewery {
+	public static void main(String[] args) {
+		// TODO
+	}
+}
