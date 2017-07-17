917f382f49d5131cc2033e30cf1b095511acce8b
ReltivlyObjectv <me@relativelyobjective.tech>
2017-07-17 13:43:39 -0700

    Removed PanelPreview and centered window

--- tech/relativelyobjective/monsterbrewery/pieces/FrameMain.java
+++ tech/relativelyobjective/monsterbrewery/pieces/FrameMain.java
@@ -11,22 +11,19 @@ import javax.swing.JFrame;
  * 
  */
 public class FrameMain extends JFrame {
-	private final PanelPreview previewWindow;
 	private final PanelMonsterOptions monsterOptions;
 	private final MenuBar menu;
 	
 	public FrameMain() {
 		super("Monster Brewery");
 		super.setLayout(new BorderLayout());
-		previewWindow = new PanelPreview();
 		monsterOptions = new PanelMonsterOptions();
 		menu = new MenuBar();
-		super.add(previewWindow, BorderLayout.CENTER);
-		super.add(monsterOptions, BorderLayout.WEST);
+		super.add(monsterOptions, BorderLayout.NORTH);
 		super.add(menu);
 		super.setJMenuBar(menu);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		super.setPreferredSize(new Dimension(1000, 600));
+		super.setPreferredSize(new Dimension(1000, 570));
 		super.setMinimumSize(super.getPreferredSize());
 		super.setMaximumSize(super.getPreferredSize());
 		super.setSize(super.getPreferredSize());
